4a3144d88a9b33a0824a3d37ed54be86fdeb0398
Takonakie <sadvan945@gmail.com>
2026-04-30 09:35:59 +0700

    fix bug: add image column to database

--- database/migrations/2026_04_28_084342_add_image_to_attendances_table.php
+++ database/migrations/2026_04_28_084342_add_image_to_attendances_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('attendances', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->after('override_reason');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('attendances', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
